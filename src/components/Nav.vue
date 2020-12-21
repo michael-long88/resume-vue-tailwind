29d9804a61ccf8d45489fa89d6806e7ba084f73f
Michael <michael.d.long88@gmail.com>
2020-12-21 17:49:43 -0500

    Add home page

---
+++ src/components/Nav.vue
@@ -0,0 +1,82 @@
+<template>
+  <!-- This example requires Tailwind CSS v2.0+ -->
+  <nav class="bg-gray-800">
+    <div class="max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div class="relative flex items-center justify-between h-16">
+        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          <!-- Mobile menu button-->
+          <button class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-expanded="false">
+            <span class="sr-only">Open main menu</span>
+            <!-- Icon when menu is closed. -->
+            <!--
+              Heroicon name: menu
+
+              Menu open: "hidden", Menu closed: "block"
+            -->
+            <svg @click="menuToggle = !menuToggle" :class="['h-6 w-6', menuToggle ? 'hidden' : 'block']" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+            <!-- Icon when menu is open. -->
+            <!--
+              Heroicon name: x
+
+              Menu open: "block", Menu closed: "hidden"
+            -->
+            <svg @click="menuToggle = !menuToggle" :class="['h-6 w-6', menuToggle ? 'block' : 'hidden']" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          </button>
+        </div>
+        <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+          <div class="hidden sm:block sm:ml-6">
+            <div class="flex space-x-4">
+              <p class="menu-link-text text-white my-auto">Michael Long's Resume</p>
+              <p class="mx-auto text-3xl">|</p>
+              <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+              <router-link to="/" class="menu-link text-sm">Home</router-link>
+              <p class="mx-auto text-3xl">|</p>
+              <router-link to="/about" class="menu-link text-sm">About</router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!--
+      Mobile menu, toggle classes based on menu state.
+
+      Menu open: "block", Menu closed: "hidden"
+    -->
+    <div :class="[menuToggle ? 'block sm:block' : 'hidden sm:hidden']">
+      <div class="px-2 pt-2 pb-3 space-y-1">
+        <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+        <router-link to="/" class="menu-link text-base">Home</router-link> |
+        <router-link to="/about" class="menu-link text-base">About</router-link>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+  data () {
+    return {
+      menuToggle: false
+    }
+  }
+}
+</script>
+<style scoped>
+@layer base {
+  .menu-link-text {
+    @apply rounded-md font-medium;
+  }
+  .menu-link {
+    @apply text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 menu-link-text;
+  }
+  .menu-link.router-link-exact-active {
+    @apply bg-gray-900 text-white menu-link-text;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex h-screen w-screen">
+    <div class="m-auto grid grid-cols-2 grid-rows-2 gap-8">
+      <div>
+        <img class="m-auto rounded-md" alt="headshot" src="../assets/Long_Michael_510.jpg">
+      </div>
+      <div class="m-auto">
+        <h2 class="text-8xl px-8">Hi, I'm Michael Long, a software developer.</h2>
+      </div>
+      <div class="col-span-2 mx-auto h-8">
+        <p class="text-2xl">
+          <a href="mailto:michael.long@mlong.me" target="_blank">Email</a>
+          &nbsp;|&nbsp;
+          <a href="https://www.linkedin.com/in/michael-long88/" target="_blank">LinkedIn</a>
+          &nbsp;|&nbsp;
+          <a href="https://github.com/michael-long88" target="_blank">GitHub</a>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
