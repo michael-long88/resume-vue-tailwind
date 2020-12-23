67d184a1db047c1c5ee6519c1002321a442d9148
Michael <michael.d.long88@gmail.com>
2020-12-23 09:25:17 -0600

    Add responsive breakpoints to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="flex h-screen w-screen">
-    <div class="m-auto grid grid-cols-2 grid-rows-2 gap-8">
+  <div class="flex lg:h-screen w-full">
+    <div class="m-auto lg:grid lg:grid-cols-2 lg:grid-rows-2 lg:gap-8">
       <div>
         <img class="m-auto rounded-md" alt="headshot" src="../assets/Long_Michael_510.jpg">
       </div>
       <div class="m-auto">
-        <h2 class="text-8xl px-8">Hi, I'm Michael Long, a software developer.</h2>
+        <h2 class="text-2xl pl-2 py-4 lg:text-8xl lg:px-8">Hi, I'm Michael Long, a software developer.</h2>
       </div>
-      <div class="col-span-2 mx-auto h-8">
-        <p class="text-2xl">
+      <div class="lg:col-span-2 mx-auto lg:h-8">
+        <p class="text-base lg:text-2xl">
           <a href="mailto:michael.long@mlong.me" target="_blank">Email</a>
           &nbsp;|&nbsp;
           <a href="https://www.linkedin.com/in/michael-long88/" target="_blank">LinkedIn</a>
