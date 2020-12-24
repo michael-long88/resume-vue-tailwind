6ac6ec6c016565dd5c0a517be832767bf56bc4c5
Michael <michael.d.long88@gmail.com>
2020-12-23 21:05:17 -0600

    Tweak responsive design for home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="flex lg:h-screen w-full">
-    <div class="m-auto lg:grid lg:grid-cols-2 lg:grid-rows-2 lg:gap-8">
-      <div>
-        <img class="m-auto rounded-md" alt="headshot" src="../assets/Long_Michael_510.jpg">
+    <div class="m-auto pt-4 lg:grid lg:grid-cols-2 lg:grid-rows-2 lg:gap-8">
+      <div class="m-auto">
+        <img class="rounded-md" alt="headshot" src="../assets/Long_Michael_510.jpg">
       </div>
       <div class="m-auto">
-        <h2 class="text-2xl pl-2 py-4 lg:text-8xl lg:px-8">Hi, I'm Michael Long, a software developer.</h2>
+        <h2 class="text-2xl pl-2 py-4 text-center lg:text-left lg:text-8xl lg:px-8">Hi, I'm Michael Long, a software developer.</h2>
       </div>
       <div class="lg:col-span-2 mx-auto lg:h-8">
-        <p class="text-base lg:text-2xl">
+        <p class="text-base text-center lg:text-2xl">
           <a href="mailto:michael.long@mlong.me" target="_blank">Email</a>
           &nbsp;|&nbsp;
           <a href="https://www.linkedin.com/in/michael-long88/" target="_blank">LinkedIn</a>
