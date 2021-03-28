20ae39e8b8d1ac1906b8fc81d80977b8670cb382
Michael <michael.d.long88@gmail.com>
2021-03-28 10:22:14 -0400

    Extract jobs and skills objects to separate files

---
+++ src/skillsList.js
@@ -0,0 +1,31 @@
+export const skillsList = {
+  Technologies: ['Python', 'HTML5/CSS', 'JavaScript', 'Vue.js', 'Highcharts', 'SQL', 'Java', 'Windows', 'MacOS', 'Linux'],
+  Communication: [
+    'Demonstrated features and changes from multiple projects to clients and co-workers.',
+    'Presented changes to projects and gave recommended courses of action to clients.',
+    'Presented detailed plans to individuals in charge as well as subordinates.',
+    'Communicated effectively with locals in foreign countries using freshly learned phrases and communication skills.',
+    'Assisted faculty and students in resolving technical issues ranging from password resets to network issues.'
+  ],
+  Leadership: [
+    'Acted as lead developer on data visualization dashboard project.',
+    'Led 12 individuals in both a tactical and home environment.',
+    'Mentored junior Marines to help them grow both personally and professionally.',
+    'Supervised Marine Corps tasks applied to the fire team, squad, platoon, and company level.'
+  ],
+  'Time Management': [
+    'Created task and timeline definitions for project completion.',
+    'Accomplished numerous tasks in a set schedule, as well as assisted in the establishment of timelines.',
+    'Performed under pressure in a condensed time line on numerous occasions.'
+  ],
+  Adaptability: [
+    'Update project features based on client feedback in an agile environment.',
+    'Adapted to the fluid and ever-changing military environment while overseas and stateside.',
+    'Incorporated new technologies to enhance existing projects.'
+  ],
+  Teamwork: [
+    'Collaborated and coordinated on projects across internal and external teams.',
+    'Worked with 7 other interns from 4 separate locations over the course of a 2 month internship.',
+    'Utilized Agile methodologies in a team environment.'
+  ]
+}
