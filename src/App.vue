7bafae7d424135930c40bd0507e9caad2803721b
Michael <michael.d.long88@gmail.com>
2021-03-28 09:23:25 -0400

    Remove unnecessary code

--- src/App.vue
+++ src/App.vue
@@ -32,24 +32,5 @@ export default {
       }
     }
   }
-  // created () {
-  //   // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-  //   if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-  //     document.querySelector('html').classList.add('dark')
-  //     document.querySelector('html').classList.add('bg-gray-600')
-  //   } else {
-  //     document.querySelector('html').classList.remove('dark')
-  //     document.querySelector('html').classList.remove('bg-gray-600')
-  //   }
-
-  //   // Whenever the user explicitly chooses light mode
-  //   localStorage.theme = 'light'
-
-  //   // Whenever the user explicitly chooses dark mode
-  //   localStorage.theme = 'dark'
-
-  //   // Whenever the user explicitly chooses to respect the OS preference
-  //   localStorage.removeItem('theme')
-  // }
 }
 </script>
