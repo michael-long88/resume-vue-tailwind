d7d54f0d9c6b59da9d0766c7cda74e734b8704e2
Michael <michael.d.long88@gmail.com>
2021-03-28 13:18:25 -0400

    Comment out created and destroyed methods on resume page

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="flex-grow overflow-auto max-h-screen w-full dark:text-gray-300">
     <div class="m-auto h-screen">
-      <div v-show="darkModePDF">
+      <div v-if="darkModePDF">
         <embed id="resume" src="LongMichael_WebResume_PhoneDarkMode.pdf" type="application/pdf" class="h-screen w-full">
       </div>
-      <div v-show="!darkModePDF">
+      <div v-else>
         <embed id="resume" src="LongMichael_WebResume.pdf" type="application/pdf" class="h-screen w-full">
       </div>
     </div>
@@ -17,13 +17,13 @@ export default {
   name: 'Resume',
   data () {
     return {
-      publicPath: process.env.BASE_URL,
       windowWidth: 0
     }
   },
   computed: {
     ...mapGetters({ theme: 'getTheme' }),
     darkModePDF () {
+      // if (this.windowWidth < 1200 && this.theme === 'dark') {
       if (this.windowWidth < 1200 && this.theme === 'dark') {
         return true
       }
@@ -34,13 +34,13 @@ export default {
     handleResize () {
       this.windowWidth = window.innerWidth
     }
-  },
-  created () {
-    window.addEventListener('resize', this.handleResize)
-    this.handleResize()
-  },
-  destroyed () {
-    window.removeEventListener('resize', this.handleResize)
   }
+  // created () {
+  //   window.addEventListener('resize', this.handleResize)
+  //   this.handleResize()
+  // },
+  // destroyed () {
+  //   window.removeEventListener('resize', this.handleResize)
+  // }
 }
 </script>
