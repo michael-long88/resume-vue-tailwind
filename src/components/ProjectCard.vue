89ce9ec2c267eabf6bdfd8cc6ae93335475cf011
Michael <michael.d.long88@gmail.com>
2022-11-01 22:18:32 -0400

    Switch project cards to hard-coded col start

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,12 +1,12 @@
 <template>
-  <div :class="'flip h-64 max-h-screen lg:col-span-4 lg:col-start-' + startColumn">
+  <div class="flip h-64 max-h-screen lg:col-span-4 lg:col-start-3">
     <div class="flip-content flex-none inline-flex flex-col">
       <div :class="'flip-front rounded overflow-hidden shadow-xl border-2 border-gray-600 flex justify-center items-center dark:shadow-xl-white dark:border-gray-700'">
         <div class="px-6 py-4">
           <div class="font-bold text-xl mb-2">{{ cardName }}</div>
         </div>
       </div>
-      <div :class="'flip-back rounded overflow-auto shadow-xl border-2 border-gray-600 lg:col-span-3 lg:col-start-' + startColumn + ' dark:shadow-xl-white dark:border-gray-700'">
+      <div class="flip-back rounded overflow-auto shadow-xl border-2 border-gray-600 lg:col-span-4 lg:col-start-3 dark:shadow-xl-white dark:border-gray-700">
         <div class="grid">
           <div class="px-6 py-2 text-center">
             <div class="font-bold text-xl mb-2">{{ cardName }}</div>
