d18a8c94f2d1a25e205482655000a89591cb6088
Michael <michael.d.long88@gmail.com>
2022-11-11 11:52:54 -0500

    Update tag line

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
         <img class="rounded-md" alt="headshot" src="../assets/Long_Michael_510.jpg">
       </div>
       <div class="m-auto">
-        <h2 class="text-2xl pl-2 py-4 text-center lg:text-left lg:text-8xl lg:px-8">Hi, I'm Michael Long, a software developer.</h2>
+        <h2 class="text-2xl pl-2 py-4 text-center lg:text-left lg:text-8xl lg:px-8">Hi, I'm Michael Long, a software developer turned data scientist.</h2>
       </div>
       <div class="lg:col-span-2 mx-auto lg:h-8">
         <p class="text-base text-center lg:text-2xl">
